da0f26e4d929e95bf0870ffeee61ff93373c880d
Kacper Kielak <kielak.kacper@gmail.com>
2020-05-24 16:25:22 +0100

    Add README.md for Kuhn Poker

--- cfre/kuhn/kuhn_trainer.py
+++ cfre/kuhn/kuhn_trainer.py
@@ -7,18 +7,6 @@ import numpy as np
 from cfre.kuhn.information_set import InformationSet
 
 
-### RULES OF THE GAME
-# 1. Two players each bet 1 chip
-# 2. There are three cards, each corresponding to one of the numbers: 1, 2, 3
-# 3. One card is dealt to each of the players
-# 4. Game starts with player one
-# 5. Game alternates between player 1 and 2
-# 6. Players can bet (1 chip only) or pass
-# 7. If player passes after bet -> opponent takes all chips
-# 8. After two consecutive passes or two consecutive bets player reveal their
-#      cards and one with higher card takes all chips.
-
-
 CARDS = [1, 2, 3]
 NUM_ACTIONS = 2
 VALUE_TO_ACTION = {0: 'p', 1: 'b'}
